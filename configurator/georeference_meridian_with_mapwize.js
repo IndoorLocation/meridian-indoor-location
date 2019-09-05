f672e20e364aba9ad9641db10c4a56d69162cfa1
saipi jasmin <jasmin.saipi@epitech.eu>
2019-09-05 17:55:21 +0200

    add doc

--- configurator/georeference_meridian_with_mapwize.js
+++ configurator/georeference_meridian_with_mapwize.js
@@ -141,3 +141,30 @@ async.series([
     process.exit(0);
   }
 });
+
+// 1) Get list of maps for Location from Meridian
+// https://edit-eu.meridianapps.com/api/locations/{{Location ID}}/maps
+// Header Authorization = Token {meridianToken}
+
+// 2) Get list of raster sources for venue in Mapwize
+
+/*
+   3) For each raster source in Mapwize with name in format "Meridian | {map name}"
+        - Get meridian {map Id} from {map name}
+        - Update gps_ref_points in the map object in Meridian 
+          PUT https://edit-eu.meridianapps.com/api/locations/{{Location ID}}/maps/{{Map ID}}
+          with just the JSON
+          {
+        	"gps_ref_points": "48.88974940602677,2.2493344986198736,48.889514709374936,2.2495410887287335,62,49.5,1366,954"
+          }
+          var gps_ref_points = [
+              georeference.points[0].longitude,
+              georeference.points[0].latitude,
+              georeference.points[1].longitude,
+              georeference.points[1].latitude,
+              georeference.points[0].x,
+              georeference.points[0].y,
+              georeference.points[1].x,
+              georeference.points[1].y
+          ]
+*/
